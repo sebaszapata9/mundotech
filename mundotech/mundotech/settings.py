a0689746ebb61d14d445dd92991f2fdde7ca869d
Sebas Zapata <zapatavergara28@gmail.com>
2025-08-02 13:41:36 -0500

    cambio en settings por nuevo modulo sql

--- mundotech/mundotech/settings.py
+++ mundotech/mundotech/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 import django_heroku
 
@@ -77,10 +78,7 @@ WSGI_APPLICATION = 'mundotech.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(BASE_DIR / 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3', conn_max_age=600, ssl_require=True)
 }
 
 
